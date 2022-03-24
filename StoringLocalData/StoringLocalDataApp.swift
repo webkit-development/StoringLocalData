4fa074e43b010f8bc1f1e2a0baed1e93ccbd4215
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-03-23 20:29:59 -0400

    Initial Commit

---
+++ StoringLocalData/StoringLocalDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoringLocalDataApp.swift
+//  StoringLocalData
+//
+//  Created by Kevin Stradtman on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct StoringLocalDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
